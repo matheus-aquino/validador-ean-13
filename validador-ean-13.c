730491efd3e42835faab0876ceff1777b354be41
Matheus <matheus.martins.r.a@gmail.com>
2026-08-31 06:43:23 -0300

    Adiciona a funcionalidade de validar o código informado

--- validador-ean-13.c
+++ validador-ean-13.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 typedef struct {
     char codigo[14]; // 13 digits + null terminator
@@ -13,6 +14,24 @@ int cadastrar(Produto **lista, int *n, const char *codigo, const char *nome);
 int buscar_por_codigo(const Produto *lista, int n, const char *codigo);
 void liberar(Produto **lista, int *n);
 
+int validar_formato(const char *codigo) {
+    if (strlen(codigo) != 13) return 0;
+    int i = 0;
+    while (codigo[i] != '\0') {
+        if (!isdigit((unsigned char)codigo[i])) {
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+}
+
 int main() {
+    Produto *lista = NULL;
+    char codigo[14] = "7891000100103";
+    if (!validar_formato(&codigo)) {
+        printf("Formato de código inválido\n");
+    }
+    
     return 0;
 }
