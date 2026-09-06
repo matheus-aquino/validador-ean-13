4ab5475bd6ad1fe7e6b63722fc4d7d68bd2c1b03
Matheus <matheus.martins.r.a@gmail.com>
2026-09-06 07:35:35 -0300

    Adiciona a funcao de menu e faz o cadastro de itens da main, testando possiveis cenarios de erro

--- validador-ean-13.c
+++ validador-ean-13.c
@@ -27,7 +27,7 @@ int validar_formato(const char *codigo) {
 }
 
 int calcular_dv(const char *codigo){
-    if (!validar_formato((codigo))) return 0;
+    if (!validar_formato((codigo))) return validar_formato(codigo);
     int tamanho = strlen(codigo) - 1;
     int soma = 0;
     int dv = 0;
@@ -42,24 +42,23 @@ int calcular_dv(const char *codigo){
     dv = ((10 - soma %10) % 10);
     if ((soma + dv) %10 != 0) {
         return -1;
+    } else if (dv != (codigo[12] - '0')) {
+        return -1;
     } else {
         return dv;
     }
 }
 
 int cadastrar(Produto **lista, int *n, const char *codigo, const char *nome) {
-    if (!validar_formato(codigo)) return 0;
     if ((*n) < 0) return 0;
+    if (validar_formato(codigo) == 0) return validar_formato(codigo);
+    if (calcular_dv(codigo) != (codigo[12] - '0')) return calcular_dv(codigo);
+    if (buscar_por_codigo(*lista, *n, codigo) != -2) return -2;
 
-    
     Produto *tmp = realloc(*lista, (*n + 1) * sizeof(*tmp));
     if (tmp == NULL) return 0;
 
     (*lista) = tmp;
-
-    for (int i = 0; i < (*n); i++) {
-        if (strcmp((*lista)[i].codigo, codigo) == 0) return -2;
-    }
 
     strcpy((*lista)[(*n)].codigo, codigo);
     strcpy((*lista)[(*n)].nome, nome);
@@ -76,7 +75,28 @@ int buscar_por_codigo(const Produto *lista, int n, const char *codigo) {
             return i;
         }
     }
-    return -1;
+    return -2;
+}
+
+void menu (const int cadastro_resultado, const char *codigo, const char *nome) {
+    switch (cadastro_resultado)
+    {
+    case -2:
+        printf("REJEITADO: %s (JA CADASTRADO)\n", codigo);
+        break;
+    case -1:
+        printf("REJEITADO: %s (DV INVALIDO, ESPERADO %c)\n", codigo, codigo[12]);
+        break;
+    case 0:
+        printf("REJEITADO: %s (FORMATO INVALIDO)\n", codigo);
+        break;
+    case 1:
+        printf("CADASTRADO: %s %s\n", codigo, nome);
+        break;
+    default:
+        break;
+    }
+
 }
 
 void liberar(Produto **lista, int *n) {
@@ -88,28 +108,49 @@ void liberar(Produto **lista, int *n) {
 int main() {
     Produto *lista = NULL;
     int tamanho = 0;
-    //char codigo[14] = "7891000100103";
-    /*
-    if (!validar_formato(&codigo)) {
-        printf("Formato de código inválido\n");
+
+    printf("=== VALIDADOR EAN-13 ===\n");
+    char codigo_parametro[14] = "7891000100103";
+    char nome_parametro[31] = "ARROZ 5KG";
+    int cadastro_resultado = cadastrar(&lista, &tamanho, codigo_parametro, nome_parametro);
+    menu(cadastro_resultado, codigo_parametro, nome_parametro);
+
+    strcpy(codigo_parametro, "7891000200209");
+    strcpy(nome_parametro, "FEIJAO 1KG");
+    cadastro_resultado = cadastrar(&lista, &tamanho, codigo_parametro, nome_parametro);
+    menu(cadastro_resultado, codigo_parametro, nome_parametro);
+
+    strcpy(codigo_parametro, "7898765432109");
+    strcpy(nome_parametro, "CAFE 500G");
+    cadastro_resultado = cadastrar(&lista, &tamanho, codigo_parametro, nome_parametro);
+    menu(cadastro_resultado, codigo_parametro, nome_parametro);
+
+    strcpy(codigo_parametro, "789100030030");
+    strcpy(nome_parametro, "OLEO 900ML");
+    cadastro_resultado = cadastrar(&lista, &tamanho, codigo_parametro, nome_parametro);
+    menu(cadastro_resultado, codigo_parametro, nome_parametro);
+
+    strcpy(codigo_parametro, "78910001A0103");
+    strcpy(nome_parametro, "ACUCAR 1KG");
+    cadastro_resultado = cadastrar(&lista, &tamanho, codigo_parametro, nome_parametro);
+    menu(cadastro_resultado, codigo_parametro, nome_parametro);
+
+    strcpy(codigo_parametro, "7891000100103");
+    strcpy(nome_parametro, "ARROZ REPETIDO");
+    cadastro_resultado = cadastrar(&lista, &tamanho, codigo_parametro, nome_parametro);
+    menu(cadastro_resultado, codigo_parametro, nome_parametro);
+
+    printf("PRODUTOS CADASTRADOS: %d\n", tamanho);
+    
+    int busca = buscar_por_codigo(lista, tamanho, "7891000200209");
+    if (busca != -2) {
+        printf("BUSCA %s: INDICE %d %s\n", lista[busca].codigo, busca, lista[busca].nome);
+    } else {
+        printf("BUSCA %s: NAO ENCONTRADO\n", codigo_parametro);
     }
-    */
-
-    //int dv = calcular_dv(&codigo);
-    //printf("O DV é %d\n", dv);
-
-    if (cadastrar(&lista, &tamanho, "7891000100103", "ARROZ 5KG") == -2) printf("Produto já existe\n");
-    if (cadastrar(&lista, &tamanho, "7891000200209", "FEIJAO 1KG") == -2) printf("Produto já existe\n");
-    if (cadastrar(&lista, &tamanho, "7891000200209", "FEIJAO 1KG") == -2) printf("Produto já existe\n");
-    printf("Produto %s | Nome: %s \n", lista[0].codigo, lista[0].nome);
-    printf("Produto %s | Nome: %s \n", lista[1].codigo, lista[1].nome);
-
-    int index_codigo = buscar_por_codigo(lista, tamanho, "7891000100103");
-    printf("O codigo 7891000100103 está no index %d\n", index_codigo);
-
-    index_codigo = buscar_por_codigo(lista, tamanho, "78910002002091");
-    printf("O codigo 78910002002091 está no index %d\n", index_codigo);
-
+    
     liberar(&lista, &tamanho);
+    printf("LISTA LIBERADA: %d PRODUTOS\n", tamanho);
+    printf("=== FIM ===\n");
     return 0;
 }
